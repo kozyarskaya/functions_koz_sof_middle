552119b10577b09ac21d575c5ce73492ff8ea6e9
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2021-10-13 14:52:10 +0300

    Update itc_mirror_count.cpp

--- itc_mirror_count.cpp
+++ itc_mirror_count.cpp
@@ -3,14 +3,14 @@
 int itc_mirror_count(long long number){
     int kol;
     if (number > 0){
-    for (int i = 1; i <= number; i++){
+    for (long long i = 1; i <= number; i++){
     //bool r = itc_mirror_num(i);
     if (itc_mirror_num(i))
         kol++;
         }
       }
     else{
-    for (int i = number ; i <= 1; i++){
+    for (long long i = number ; i <= 1; i++){
     //bool r = itc_mirror_num(i);
     if (itc_mirror_num(i)){
         kol++;
